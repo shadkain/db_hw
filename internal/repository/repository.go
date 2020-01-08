dccd25dfae5af2758c2b2c208339d350dcc9f2f5
shadkain <kryzev@gmail.com>
2020-01-08 22:58:59 +0300

    added repo

---
+++ internal/repository/repository.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"db_hw/internal/models"
+	"github.com/jackc/pgx"
+	"time"
+)
+
+type repositoryImpl struct {
+	connectionString string
+	db               *pgx.ConnPool
+}
+
+func NewRepository(db *pgx.ConnPool) Repository {
+	return &repositoryImpl{
+		db: db,
+	}
+}
+
+type Repository interface {
+	InsertForum(newForum models.NewForum) (Err error)
+	SelectForumsBySlug(slug string) (forum []models.Forum, Err error)
+
+	InsertPost(newPost models.NewPost, id int, forum string, created time.Time) (LastID int, Thread int, Err error)
+	SelectPostByID(ID int) (Post models.Post, Err error)
+	SelectPostByIDThreadID(ID int, threadID int) (Post models.Post, Err error)
+	SelectPosts(threadID int, limit, since, sort, desc string) (Posts *models.Posts, Err error)
+	UpdatePost(changePost models.ChangePost, postID int) (Err error)
+
+	InsertThread(newThread models.NewThread, forum string) (LastID int, Err error)
+	UpdateThread(changeThread models.ChangeThread, id int) (Err error)
+	SelectThreadsBySlug(slug string) (threads *models.Threads, Err error)
+	SelectThreadsByID(id int) (threads *models.Threads, Err error)
+	SelectThreadsByForum(forum string, limit string, since string, desc string) (Threads *models.Threads, Err error)
+
+	InsertUser(newUser models.NewUser, nickname string) (Err error)
+	SelectUsersByForum(slug, limit, since, desc string) (Users *models.Users, Err error)
+	SelectUserByNickname(nickname string) (user models.User, Err error)
+	SelectUsersByEmail(email string) (Users []models.User, Err error)
+	SelectUsersByNicknameOrEmail(email string, nickname string) (Users []models.User, Err error)
+	UpdateUser(newProfile models.NewUser, nickname string) (Err error)
+
+	InsertVote(newVote models.NewVote, id int) (Err error)
+	UpdateVote(newVote models.NewVote, id int) (Err error)
+
+	SelectStatus() (Status models.Status, Err error)
+	ClearAll() (Err error)
+}
